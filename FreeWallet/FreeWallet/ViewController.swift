b9f4511be9edd479ff5cfa7e2cd9f707d3641780
CesarGOC <cesar@MacBook-Air-de-Cesar.local>
2023-08-03 11:37:26 -0600

    comenzando programación de balance

--- FreeWallet/FreeWallet/ViewController.swift
+++ FreeWallet/FreeWallet/ViewController.swift
@@ -6,13 +6,19 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
+    
+    var pieChart = pieChartView()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 
 
 }
